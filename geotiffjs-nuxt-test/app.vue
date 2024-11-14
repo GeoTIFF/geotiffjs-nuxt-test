2b60df5a8b5b545359053d903279a2641b0bf88f
DanielJDufour <daniel.j.dufour@gmail.com>
2024-11-14 09:28:51 -0800

    example

---
+++ geotiffjs-nuxt-test/app.vue
@@ -0,0 +1,14 @@
+<script>
+import { fromUrl } from "geotiff";
+
+const tif = await fromUrl("https://storage.googleapis.com/pdd-stac/disasters/hurricane-harvey/0831/20170831_172754_101c_3b_Visual.tif");
+console.log("tif:", tif);
+
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
